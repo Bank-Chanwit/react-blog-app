cb18ac7f9aadb1e4a3d7d4a9aac09c825e6fbfe6
Bank-Chanwit <ssamsungj7299@gmail.com>
2026-02-23 16:12:30 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Home from "./components/Home";
 import About from "./components/About";
 import Blogs from "./components/Blogs";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Blogs/>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/About" element={<About/>}></Route>
+        <Route path="/Blogs" element={<Blogs/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+export default function Navbar(){
+    return(
+        <nav>
+            <Link to="/" className="logo">
+                <h3>Blog Application</h3>
+            </Link>
+            <Link to="/">หน้าแรก</Link>
+            <Link to="/about">เกียวกับเรา</Link>
+            <Link to="/blogs">บทความทั้งหมด</Link>
+        </nav>
+    )
+}
