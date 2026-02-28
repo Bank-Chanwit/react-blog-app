58b03317739b1c709a59b30636f2492708831612
Bank-Chanwit <ssamsungj7299@gmail.com>
2026-02-28 19:27:31 +0700

    add blogs component 3

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,20 +1,36 @@
-import "./Blogs.css"
+import "./Blogs.css";
 import blogs from "../data/blogs";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 export default function Blogs() {
-    return (
-        <div className="blogs-container">
-            <article>
-                {blogs.map((item) => (
-                    <Link to={`/blog/${item.id}`} key={item.id}>
-                        <div className="card" key={item.id}>
-                            <h2>{item.title}</h2>
-                            <p>{item.content.substring(0, 300)}</p>
-                            <hr />
-                        </div>
-                    </Link>
-                ))}
-            </article>
-        </div>
-    )
+  const [search, setSearch] = useState("");
+  const [filter, setfilter] = useState([]);
+  useEffect(()=>{
+    const result = blogs.filter((item)=>item.title.includes(search));
+    setfilter(result)
+  },[search]);
+  return (
+    <div className="blogs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        ></input>
+      </div>
+      <article>
+        {filter.map((item) => (
+          <Link to={`/blog/${item.id}`} key={item.id}>
+            <div className="card" key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.content.substring(0, 300)}</p>
+              <hr />
+            </div>
+          </Link>
+        ))}
+      </article>
+    </div>
+  );
 }
